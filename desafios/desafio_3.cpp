c181834840eac74396c8bc84bd25b9c982f10d5d
Daniel B <128756372+Daniel-B-Oliveira@users.noreply.github.com>
2024-06-30 15:08:55 -0300

    Update desafio_3.cpp

--- desafios/desafio_3.cpp
+++ desafios/desafio_3.cpp
@@ -9,10 +9,8 @@ int main(){
     for(i = 1; i <= T; i++){
         scanf("%f %f %f %f", &PA, &PB, &G1, &G2);
         while(PA <= PB){
-            PA = PA*(1+G1/100);
-            PA = (int)PA;
-            PB = PB*(1+G2/100);
-            PB = (int)PB;
+            PA = (int)PA*(1+G1/100);
+            PB = (int)PB*(1+G2/100);
             anos ++;
         }
         printf("%d\n", anos);
